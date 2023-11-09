e0235181fc85f53120faba0d037cab1b6a6682f3
Nguyen Van Hung <nv-hung@linkstaff.co.jp>
2023-11-09 16:16:10 +0900

    add

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    cors({
+      origin: '*',
+      method: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'x-xsrf-token'],
+      exposedHeaders: ['Content-Type'],
+    }),
+  );
   const PORT = process.env.PORT;
   await app.listen(PORT, () => {
     console.log(`server is running in http://localhost:${PORT}`);
